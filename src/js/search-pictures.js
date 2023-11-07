e818818daa35f5712e6c59cfcac37ae426c78eaa
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-11-07 12:00:02 +0200

    add libraries

---
+++ src/js/search-pictures.js
@@ -0,0 +1,6 @@
+import { Axios } from 'axios';
+import 'axios/dist/';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
