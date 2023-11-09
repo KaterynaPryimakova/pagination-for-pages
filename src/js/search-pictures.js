a83c073b54dae56825e1c7e20edb3c250d1c624a
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-11-09 13:13:58 +0200

    pagination&refactoring code

--- src/js/search-pictures.js
+++ src/js/search-pictures.js
@@ -6,18 +6,19 @@ import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 const searchForm = document.querySelector('.js-search-form');
 const input = document.querySelector('input[name="searchQuery"]');
-const searchBtn = document.querySelector('.js-search-btn');
+const serchBtn = document.querySelector('.js-search-btn');
 const gallery = document.querySelector('.js-gallery');
+const loadMoreBtn = document.querySelector('.js-load-more');
 
-searchForm.addEventListener('submit', handleSubmit);
+let page = 1;
+loadMoreBtn.style.display = 'none';
 
-async function handleSubmit(evt) {
-  evt.preventDefault();
-  const BASE_URL = 'https://pixabay.com/api';
-  const userQuery = input.value;
-  let page = 1;
-  const result = await axios
-    .get(BASE_URL, {
+async function getResponse() {
+  try {
+    const BASE_URL = 'https://pixabay.com/api';
+    const userQuery = input.value;
+
+    const response = await axios.get(BASE_URL, {
       params: {
         key: '40508767-6cd82c1efe9e2d82c03812311',
         q: userQuery,
@@ -27,25 +28,38 @@ async function handleSubmit(evt) {
         orientation: 'horizontal',
         safesearch: true,
       },
-    })
-    .then(function (response) {
-      console.log(response.data);
-
-      const resultData = response.data.hits;
-
-      if (resultData.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      gallery.innerHTML = createMarkup(resultData);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
     });
+
+    //   const response = await responsePromise.json();
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+searchForm.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const response = await getResponse();
+  const resultData = response.data.hits;
+
+  if (resultData.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  gallery.innerHTML = createMarkup(resultData);
+
+  const lightbox = new SimpleLightbox('.js-gallery a', {
+    captionDelay: 250,
+  });
+
+  loadMoreBtn.style.display = 'block';
+
+  loadMoreBtn.addEventListener('click', loadMore);
 }
 
 function createMarkup(arr) {
@@ -61,7 +75,7 @@ function createMarkup(arr) {
         downloads,
       }) => {
         return `<div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" width="320" />
+          <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width="320" /></a>
           <div class="info">
             <p class="info-item">
               <b>Likes</b> ${likes}
@@ -80,4 +94,22 @@ function createMarkup(arr) {
       }
     )
     .join('');
+}
+
+async function loadMore() {
+  const response = await getResponse();
+  console.log(response);
+
+  const resultData = response.data.hits;
+
+  gallery.insertAdjacentHTML('beforeend', createMarkup(resultData));
+
+  const currentPage = response.config.params.page;
+  const totalPages = response.data.total;
+  console.log(currentPage);
+  console.log(totalPages);
+
+  if (currentPage === totalPages) {
+    loadMoreBtn.style.display = 'none';
+  }
 }
