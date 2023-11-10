26b89f649b327f8d53b636b7a189cb12802786d6
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-11-10 21:29:14 +0200

    fixed ending of request bug

--- src/js/search-pictures.js
+++ src/js/search-pictures.js
@@ -16,9 +16,9 @@ loadMoreBtn.addEventListener('click', loadMore);
 
 searchBtn.disabled = true;
 loadMoreBtn.style.display = 'none';
-let page = 1;
+let numPage = 10;
 
-async function getResponse() {
+async function getResponse(numPage) {
   try {
     const BASE_URL = `https://pixabay.com/api/`;
     const userQuery = input.value;
@@ -27,7 +27,7 @@ async function getResponse() {
       params: {
         key: '40508767-6cd82c1efe9e2d82c03812311',
         q: userQuery,
-        page: page++,
+        page: numPage,
         per_page: 40,
         image_type: 'photo',
         orientation: 'horizontal',
@@ -60,11 +60,12 @@ async function handleSubmit(evt) {
     return;
   }
 
-  const response = await getResponse();
+  const response = await getResponse(numPage++);
   console.log(response);
+  console.log(page);
   const resultData = response.data.hits;
   const totalHits = response.data.totalHits;
-  console.log(totalHits);
+  console.log('totalHits:', totalHits);
 
   if (resultData.length === 0) {
     loadMoreBtn.style.display = 'none';
@@ -90,14 +91,16 @@ async function handleSubmit(evt) {
 function changeBtn() {
   if (input.value.trim() === '') {
     searchBtn.disabled = true;
+    loadMoreBtn.disabled = true;
   } else {
     searchBtn.disabled = false;
+    loadMoreBtn.disabled = false;
   }
 }
 
 async function loadMore() {
   try {
-    const response = await getResponse();
+    const response = await getResponse(numPage++);
     console.log(response);
 
     const resultData = response.data.hits;
@@ -105,12 +108,16 @@ async function loadMore() {
     gallery.insertAdjacentHTML('beforeend', createMarkup(resultData));
 
     const currentPage = response.config.params.page;
-    const totalPages = response.data.total;
+    const totalPages = Math.round(response.data.totalHits / 40);
     console.log(currentPage);
     console.log(totalPages);
 
     if (currentPage === totalPages) {
       loadMoreBtn.style.display = 'none';
+      Notiflix.Report.info(
+        '<(^=^)>',
+        "We're sorry, but you've reached <br/><br/> the end of search results."
+      );
     }
   } catch (error) {
     console.log(error);
